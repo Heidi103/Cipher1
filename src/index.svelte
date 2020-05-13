303859bf7db794b87a0bea749d2428601d75fc37
Heidi <Caughley.h@rangiruru.school.nz>
2020-05-13 12:20:08 +1200

    Added decrypt function
    Tested on expected inputs `testing` `9999``aaaaa`
    and boundary '' '99|99|99' '99|99|99| and clicking incorrect buttons
    All tests passed as expected

--- src/index.svelte
+++ src/index.svelte
@@ -1,26 +1,43 @@
 <script>
-	let input = ''
-  let output = ''
+  let input = "";
+  let output = "";
 
   function encrypt() {
-    let plaintext = input
-    let ciphertext = ''
-		
+    let plaintext = input;
+    let ciphertext = "";
+
     for (let i = 0; i < plaintext.length; i++) {
-      ciphertext += plaintext.charCodeAt(i) + '|'
+      ciphertext += plaintext.charCodeAt(i) + "|";
     }
-		
-    output = ciphertext
+
+    output = ciphertext;
+  }
+  function decrypt() {
+    console.log(`Decrypt button was click... `);
+    let ciphertext = input;
+    let plaintext = "";
+    let charCode = "";
+
+    for (let i = 0; i < ciphertext.length; i++) {
+      if (ciphertext[i] !== "|") {
+        charCode += ciphertext[i];
+      } else {
+        plaintext += String.fromCharCode(charCode);
+        charCode = "";
+      }
+    }
+    output = plaintext
   }
 </script>
 
 <h1>Cipher</h1>
 
 <label>
-	Text:
-	<input bind:value={input}>
+  Text:
+  <input bind:value={input} />
 </label>
 <button on:click={encrypt}>Encrypt</button>
+<button on:click={decrypt}>Decrypt</button>
 
 <h2>Result:</h2>
 <p>{output}</p>
