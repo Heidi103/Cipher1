73f5ca0535eebce2e3e4be4e71a47e4c701767d4
Heidi <Caughley.h@rangiruru.school.nz>
2020-05-13 13:51:45 +1200

    Created analysis function to help decrypting unknow shifts

--- src/index.svelte
+++ src/index.svelte
@@ -1,7 +1,8 @@
 <script>
-const shift = 3
+  const shift = 3;
   let input = "";
   let output = "";
+
 
   function encrypt() {
     let plaintext = input;
@@ -9,9 +10,9 @@ const shift = 3
 
     for (let i = 0; i < plaintext.length; i++) {
       //ciphertext += plaintext.charCodeAt(i) + "|";
-      let charCode = plaintext.charCodeAt(i)
-      charCode += shift
-      ciphertext += charCode + '|'
+      let charCode = plaintext.charCodeAt(i);
+      charCode += shift;
+      ciphertext += charCode + "|";
     }
 
     output = ciphertext;
@@ -26,24 +27,65 @@ const shift = 3
       if (ciphertext[i] !== "|") {
         charCode += ciphertext[i];
       } else {
-          charCode -= shift
+        charCode -= shift;
         plaintext += String.fromCharCode(charCode);
         charCode = "";
       }
     }
-    output = plaintext
+    output = plaintext;
+  }
+  function analyse() {
+    console.log("Analyse button has been clicked...");
+    let ciphertext = input;
+    let charCode = "";
+    let codes = [];
+    let analysis = ''
+
+    for (let i = 0; i < ciphertext.length; i++) {
+      if (ciphertext[i] !== "|") {
+        charCode += ciphertext[i];
+      } else {
+        codes = [...codes, charCode];
+        charCode = "";
+      }
+    }
+    if (codes.length) {
+      codes.sort();
+
+      let currentCode = codes;
+      let count = 0;
+
+      for (let i = 0; i < codes.length; i++) {
+        if (codes[i] === currentCode) {
+          count++;
+        } else {
+          analysis += currentCode + " appears " + count + " times.";
+          currentCode = codes[i];
+          count = 1;
+        }
+      }
+      analysis += currentCode + ' appears'  + count + ' times. ';
+    }
+    output = analysis;
   }
 </script>
+
 <section class="section content">
-<h1>Cipher</h1>
+  <h1>Cipher</h1>
 
-<label class="label">
-  Text:
-  <input class="input" bind:value={input} />
-</label>
-<button  class="button is-success is-outlined" on:click={encrypt}>Encrypt</button>
-<button class="button is-warning is-outlined" on:click={decrypt}>Decrypt</button>
+  <label class="label">
+    Text:
+    <input class="input" bind:value={input} />
+  </label>
+  <button class="button is-success is-outlined" on:click={encrypt}>
+    Encrypt
+  </button>
+  <button class="button is-warning is-outlined" on:click={decrypt}>
+    Decrypt
+  </button>
+  <button class="button" on:click={analyse}>Analyse</button>
 
-<h2>Result:</h2>
-<p>{output}</p>
+  <h2>Result:</h2>
+  <p>{output}</p>
 </section>
+
